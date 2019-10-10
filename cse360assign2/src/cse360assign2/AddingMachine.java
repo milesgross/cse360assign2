fda26b95ef0eb73bf0bb4f1e2baca36ac197b8fc
Miles <gomiles@msn.com>
2019-10-09 23:09:29 -0700

    Final

--- cse360assign2/src/cse360assign2/AddingMachine.java
+++ cse360assign2/src/cse360assign2/AddingMachine.java
@@ -12,31 +12,43 @@ public class AddingMachine {
 	/*
 	 * @author Miles Gross
 	 * @version 1.0 10/9/19
-	 * param total
+	 * @param total total of numbers being added or subtracted
+	 * @param stringTotal history of operations
 	 */
 	private int total;
+	private String stringTotal;
 	
+	//Constructor that sets up a total and a stringTotal to be used
 	public AddingMachine () {
 		total = 0;	// not needed - included for clarity
+		stringTotal = "" + total;	//base string with starting total
 	}
 	
 	public int getTotal () {
-		return 0;
+		//@return total
+		return total;
 	}
 	
+	//adds parameter to total
 	public void add (int value) {
-		
+		total = total + value;	//adds value to total
+		stringTotal = stringTotal + " + " + value;	//adds the addition operation and value to string
 	}
 	
+	//subtracts parameter to total
 	public void subtract (int value) {
-		
+		total = total - value;	//subs value from total
+		stringTotal = stringTotal + " - " + value;	//adds the subtraction operation and value to string
 	}
 	
 	public String toString () {
-		return "";
+		//@return stringTotal
+		return stringTotal;
 	}
 	
+	//resets the AddingMachine
 	public void clear () {
-		
+		total = 0;	//total reset to zero
+		stringTotal = "" + total;	//stringTotal reset to starting total
 	}
 }
