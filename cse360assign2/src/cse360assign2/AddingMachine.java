b564cdfd8d356c1016aa7c5f2432bcf2430c19bc
Miles <gomiles@msn.com>
2019-10-09 22:43:53 -0700

    First Commit

---
+++ cse360assign2/src/cse360assign2/AddingMachine.java
@@ -0,0 +1,42 @@
+/*
+ * Miles Gross
+ * CSE 360 Mon 9:40
+ * Assignment 2
+ * This class contains and AddingMachine constructor with methods
+ * to get the total, add, subtract, convert to string, and clear the machine
+ */
+
+package cse360assign2;
+
+public class AddingMachine {
+	/*
+	 * @author Miles Gross
+	 * @version 1.0 10/9/19
+	 * param total
+	 */
+	private int total;
+	
+	public AddingMachine () {
+		total = 0;	// not needed - included for clarity
+	}
+	
+	public int getTotal () {
+		return 0;
+	}
+	
+	public void add (int value) {
+		
+	}
+	
+	public void subtract (int value) {
+		
+	}
+	
+	public String toString () {
+		return "";
+	}
+	
+	public void clear () {
+		
+	}
+}
